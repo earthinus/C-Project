458276a53f647a72a919c7025d23acc5163948d3
maki toda <earth.in.us@gmail.com>
2017-02-02 18:08:37 -0800

    Default project files of Xcode.

---
+++ C-Project/C-Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-Project
+//
+//  Created by Mai & Maki on 2017-02-02.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
